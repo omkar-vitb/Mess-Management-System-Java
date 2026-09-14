8e5124442772f8607a3280b877df9072bac7f6c8
omkar-vitb <omkar.25bai10893@vitbhopal.ac.in>
2026-09-14 23:40:30 +0530

    Update DuplicateSerialException.java

--- DuplicateSerialException.java
+++ DuplicateSerialException.java
@@ -1,3 +1,5 @@
+// This java file helps in preventing in creatig two id's. whenevr anyone tries to enter a student id that already existes in the database this file helps in flagging out the problem instead of letting the entire system crash
+
 public class DuplicateSerialException extends Exception {
     public DuplicateSerialException(String message){
         super(message);
