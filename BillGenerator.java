ebb3a89da1d1dd6215125eda98bc80ed949f3597
omkar-vitb <omkar.25bai10893@vitbhopal.ac.in>
2026-09-14 23:38:38 +0530

    Update BillGenerator.java

--- BillGenerator.java
+++ BillGenerator.java
@@ -1,3 +1,5 @@
+// This file can be used to create a bill in .txt file to act as physical receipt.
+
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,36 +10,39 @@ import java.io.IOException;
             String fileName=s.getName().replaceAll(" ","_")+"Bill.txt";
 
             try(FileWriter writer=new FileWriter(fileName)){
-                writer.write("=================================");
-                System.out.println();
-                writer.write("MESS MONTHLY INVOICE");
-                System.out.println();
+                writer.write("=================================\n");
+                
+                writer.write("MESS MONTHLY INVOICE\n");
+                
 
-            writer.write("=====================================");
-            System.out.println();
-            writer.write("Student Name: "+s.getName());
-            System.out.println();
-            writer.write("Serial No: "+s.getSerialNo());
-            System.out.println();
-            writer.write("Join Date: "+s.getJoinDate());
-            System.out.println();
-            writer.write("Lunch Included: " +(s.includeLunch()?"Yes":"No"));
-            System.out.println();
-            writer.write("Dinner Included: "+(s.includeDinner()?"Yes":"No"));
-            System.out.println();
-            writer.write("-------------------------------------");
-            System.out.println();
-            writer.write("TOTAL AMOUNT DUE: Rs."+s.getMonthlyPrice());
-            System.out.println();
-            writer.write("=====================================");
-            System.out.println();
+            writer.write("=====================================\n");
+           
+            writer.write("Student Name: "+s.getName()+"\n");
+            
+            writer.write("Serial No: "+s.getSerialNo()+"\n");
+           
+            writer.write("Join Date: "+s.getJoinDate()+"\n");
+            
+            writer.write("Lunch Included: " +(s.includeLunch()?"Yes":"No")+"\n");
+            
+            writer.write("Dinner Included: "+(s.includeDinner()?"Yes":"No")+"\n");
+            
+            writer.write("-------------------------------------\n");
+            
+            writer.write("TOTAL AMOUNT DUE: Rs."+s.getMonthlyPrice()+"\n");
+            
+            writer.write("=====================================\n");
+            
 
             System.out.println("Success! Bill saved as "+fileName+" in the project folder.");
         }catch (IOException e){
             System.out.println("Error generating bill: "+e.getMessage());
             
             }
+            
         }
+
+
     }
     
 
