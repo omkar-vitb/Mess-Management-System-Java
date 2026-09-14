4f81728c1926fb9804b8aea573677c3ed1e98251
omkar-vitb <omkar.25bai10893@vitbhopal.ac.in>
2026-09-14 23:39:20 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,16 +3,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 
-
-public class DBConnection {
+// this java file acts as the bridge that links the java application to the local MYSQL server.
+public class DBConnection{
     private static final String URL= "jdbc:mysql://localhost:3306/MessManagementDB";
-
     private static final String USER="root";
-    private static final String PASSWORD = "Vitadm@232005";
-    //put your your password here i have hid it for security purpose.
+
+    private static final String 
+
+    //put your your password here, i have hide mine  for security purpose.
+    PASSWORD="Enter you mysql password";
+    
 
     public static Connection getConnection() throws SQLException{
+
         return DriverManager.getConnection(URL, USER,PASSWORD);
 
     }   
+
+
 }
